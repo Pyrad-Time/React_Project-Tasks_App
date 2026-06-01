043089c8017b98a55ff4a72be603f3f177a38103
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-01 09:33:05 -0300

    feat(taskList): render tasks cards from task state

--- front-end/src/components/TaskList/TaskList.jsx
+++ front-end/src/components/TaskList/TaskList.jsx
@@ -1,2 +1,17 @@
 // Component responsible for receiving the tasks list
 // and rendiring each task on the screen
+import TaskCard from "../TaskCard/TaskCard.jsx"
+
+export default function TaskList (props) {
+    return (
+        <section className="taskList__container">
+            <ul className="taskList__tasks">
+                {props.tasks.map((task) => {
+                    return (
+                        <TaskCard key={task.id} task={task}/>
+                    )
+                })}
+            </ul>
+        </section>
+    )
+}
