a2e2d794f1549aa9a1b7ab4f0a4c2c3cf2dcca4d
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-01 09:06:22 -0300

    feat(state): add task state management

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 import "./components/Header/Header.jsx"
 import Header from './components/Header/Header.jsx'
+import TaskForm from './components/TaskForm/TaskForm.jsx'
 // Main component responsible for storgin the tasks state
 // Coordinating the data flow between components
+
 function App() {
+
+  const [taskState, setTaskState] = useState([])
+
+  function addTask(taskTitle) {
+    const newTask = {
+      id: Date.now(),
+      title: taskTitle,
+      isCompleted: false
+    }
+
+    setTaskState((currentTask) => [...currentTask, newTask])
+
+    console.log(newTask)
+    console.log(taskState)
+  }
 
   return (
     <>
       <Header/>
+      <TaskForm onAddTask={addTask}/>
     </>
   )
 }
