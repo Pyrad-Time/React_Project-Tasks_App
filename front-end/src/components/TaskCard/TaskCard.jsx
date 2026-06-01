61265c30c2baf4d1ed9581c4d8063a5407950d63
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-01 09:36:05 -0300

    feat(taskCard): render task title from props

--- front-end/src/components/TaskCard/TaskCard.jsx
+++ front-end/src/components/TaskCard/TaskCard.jsx
@@ -1,2 +1,12 @@
 // Component responsible for displaying a single task
 // Triggering task actions such as complete or delete
+
+export default function TaskCard(props) {
+    return (
+        <li className="taskCard__item">
+            <span className="taskCard__title">
+                {props.task.title}
+            </span>
+        </li>
+    )
+}
