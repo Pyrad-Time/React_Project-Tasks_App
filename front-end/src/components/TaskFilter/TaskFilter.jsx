8f49d99699a8c9f25bbf6c03dd2976c98ded7ec8
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-07 09:53:03 -0300

    feat(taskFilter): implement task status filtering

--- front-end/src/components/TaskFilter/TaskFilter.jsx
+++ front-end/src/components/TaskFilter/TaskFilter.jsx
@@ -1,2 +1,44 @@
 // Component responsible for changing which tasks are displayed
 // all, pending or completed.
+import "./TaskFilter.css"
+
+export default function TaskFilter(props) {
+    return (
+        <section className="taskFilter__container">
+            <button 
+                onClick={() => {
+                    props.onChangeFilter("all")
+                }}
+                type="button"
+                className={
+                    props.activeFilter === "all"
+                        ? "taskFilter__button taskFilter__buttonActive" 
+                        : "taskFilter__button"}
+                >Todas</button>
+
+            <button 
+                onClick={() => {
+                    props.onChangeFilter("pending")
+                }}
+                type="button"
+                className={
+                    props.activeFilter === "pending"
+                    ? "taskFilter__button taskFilter__buttonActive"
+                    : "taskFilter__button"
+                }
+            >Pendentes</button>
+
+            <button 
+                onClick={() => {
+                    props.onChangeFilter("completed")
+                }}
+                type="button"
+                className={
+                    props.activeFilter === "completed"
+                    ? "taskFilter__button taskFilter__buttonActive"
+                    : "taskFilter__button"
+                }
+            >Concluídas</button>
+        </section>
+    )
+}
