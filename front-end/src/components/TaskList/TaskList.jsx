ff79e3973984d1dd4b2378abd0d4a7680edda7a2
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-08 09:12:28 -0300

    feat(taskList): handle empty task list

--- front-end/src/components/TaskList/TaskList.jsx
+++ front-end/src/components/TaskList/TaskList.jsx
@@ -1,21 +1,27 @@
 // Component responsible for receiving the tasks list
-// and rendiring each task on the screen
+// and rendering each task on the screen
 import TaskCard from "../TaskCard/TaskCard.jsx"
 
-export default function TaskList (props) {
+export default function TaskList(props) {
     return (
         <section className="taskList__container">
             <ul className="taskList__tasks">
-                {props.tasks.map((task) => {
-                    return (
-                        <TaskCard 
-                            key={task.id}
-                            task={task}
-                            onRemoveTask={props.onRemoveTask}
-                            onToggleTaskComplete={props.onToggleTaskComplete}
-                        />
-                    )
-                })}
+                {props.tasks.length === 0 ? (
+                    <li className="taskList__empty">
+                        Nenhuma tarefa encontrada
+                    </li>
+                ) : (
+                    props.tasks.map((task) => {
+                        return (
+                            <TaskCard 
+                                key={task.id}
+                                task={task}
+                                onRemoveTask={props.onRemoveTask}
+                                onToggleTaskComplete={props.onToggleTaskComplete}
+                            />
+                        )
+                    })
+                )}
             </ul>
         </section>
     )
