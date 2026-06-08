e88f6767344004be8d451e6db6571e0b41ced69b
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-08 09:18:03 -0300

    feat(header): add total and completed task counters

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
 // Visual component responsible for rendering the application title
 import "./Header.css"
 
-function Header() {
+function Header(props) {
     return (
         <header className="header__container">
-            <h1 className="header__title">Tasks App</h1>
-            <p className="header__subtitle">Created by: Pyrad-Time</p>
+            <div>
+                <h1 className="header__title">Tasks App</h1>
+                <p className="header__subtitle">Created by: Pyrad-Time</p>
+            </div>
+            <div className="header__statusContainer">
+                <div className="header__status">
+                    <span className="header__statusTitle">Total</span>
+                    <strong className="header__statusValue">{props.onTotalTasks}</strong>
+                </div>
+                <div className="header__status">
+                    <span className="header__statusTitle">Concluídos</span>
+                    <strong className="header__statusValue">{props.onCompletedTasks}</strong>
+                </div>
+
+            </div>
         </header>
     )
 }
