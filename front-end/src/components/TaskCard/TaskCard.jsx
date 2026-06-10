7d1a2db4c2b60e570f95eb994ce521db7fcef305
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-10 10:01:58 -0300

    refactor(taskCard): improve checkbox accessibility

--- front-end/src/components/TaskCard/TaskCard.jsx
+++ front-end/src/components/TaskCard/TaskCard.jsx
@@ -5,17 +5,32 @@ import "./TaskCard.css"
 export default function TaskCard(props) {
     return (
         <li className="taskCard__item">
-            <span className={props.task.isCompleted === true
+            <input 
+                className="taskCard__checkbox"
+                type="checkbox" 
+                id={`task-${props.task.id}`}  
+                onChange={() => 
+                    {props.onToggleTaskComplete(props.task.id)
+                }}
+            />
+
+            <label className={props.task.isCompleted === true
                 ? "taskCard__title taskCard__titleCompleted"
                 : "taskCard__title"
-            }>
+            }
+                    htmlFor={`task-${props.task.id}`}
+            >
                 {props.task.title}
-            </span>
-            <button onClick={() => props.onRemoveTask(props.task.id)}>Remove</button>
-            <input type="checkbox" id={props.task.id}  
-                onChange={() => {props.onToggleTaskComplete(props.task.id)}}
-                ></input>
-            <label htmlFor={props.task.id}></label>
+            </label>
+
+            <button 
+                className="taskCard__button"
+                type="button"
+                onClick={() => 
+                    props.onRemoveTask(props.task.id)
+                }
+            >Remove</button>
+            
         </li>
     )
 }
