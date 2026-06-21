350c0911a62958284cf7f77c717fadfd1b4e5bd5
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-20 21:44:19 -0300

    feat(tasks): add task api service

---
+++ front-end/src/services/taskService.js
@@ -0,0 +1,51 @@
+const API_URL = "http://localhost:3000/taks"
+
+async function getTasks() {
+    const response = await fetch(API_URL)
+
+    if(!response.ok) {
+        throw new Error("Erro ao buscar tarefas")
+    }
+
+    return response.json()
+}
+
+async function createTask(title) {
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title })
+    })
+
+    if(!response.ok) {
+        throw new Error("Erro ao criar tarefa")
+    }
+
+    return response.json()
+}
+
+async function deleteTask(id) {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE"
+    })
+
+    if(!response.ok) {
+        throw new Error("Erro ao deletar tarefa")
+    }
+
+    return response.json()
+}
+
+async function toggleTask(id){
+    const response = await fetch(`${API_URL}/${id}/toggle`, {
+        method: "PATCH"
+    })
+
+    if(!response.ok) {
+        throw new Error("Erro ao atualizar checkbox")
+    }
+}
+
+export { getTasks, createTask, deleteTask, toggleTask }
