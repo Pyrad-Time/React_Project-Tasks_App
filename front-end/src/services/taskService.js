8e0744ec9573569c0831ff7b57b429e04906a64e
Pyrad-Time <marcos0fataak@gmail.com>
2026-06-23 09:04:23 -0300

    chore(frontedn): support api url enviroment variable

--- front-end/src/services/taskService.js
+++ front-end/src/services/taskService.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:3000/tasks"
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000"
+const API_URL = `${API_BASE_URL}/tasks`
 
 async function getTasks() {
     const response = await fetch(API_URL)
